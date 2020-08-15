7b52657c58fc39bc09e4cca71c62126be26508bc
sottar <ohrst.18@gmail.com>
2020-08-15 13:59:17 +0900

    feat(components): button component

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -1,11 +1,63 @@
+import { LoadingOutlined } from '@ant-design/icons';
 import React from 'react';
+import styled from 'styled-components';
+
+type ButtonType = 'primary' | 'default';
 
 type ButtonProps = {
-  text: string;
+  type?: ButtonType;
+  isLoading?: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
+  className?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ text }) => {
-  return <div>{text}</div>;
+const BaseWrapper = styled.button<{ disabled?: boolean; isLoading?: boolean }>`
+  padding: 5px 16px;
+  text-align: center;
+  background: #fff;
+  border: 0;
+  border-radius: 2px;
+  opacity: ${(props) => (props.disabled || props.isLoading ? '.5' : '1')};
+  cursor: pointer;
+  transition: opacity 0.3s;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+const BaseButton: React.FC<ButtonProps> = (props) => {
+  return (
+    <BaseWrapper
+      onClick={props.onClick}
+      className={props.className}
+      disabled={props.disabled}
+      isLoading={props.isLoading}
+    >
+      {props.children}
+      {props.isLoading && <LoadingOutlined />}
+    </BaseWrapper>
+  );
+};
+
+const PrimaryButton = styled(BaseButton)`
+  color: #fff;
+  background: #1890ff;
+  border: 1px solid #1890ff;
+`;
+const DefaultButton = styled(BaseButton)`
+  color: #1890ff;
+  border: 1px solid #1890ff;
+`;
+
+const Button: React.FC<ButtonProps> = (props) => {
+  switch (props.type) {
+    case 'primary':
+      return <PrimaryButton {...props} />;
+    case 'default':
+    default:
+      return <DefaultButton {...props} />;
+  }
 };
 
 export default Button;
