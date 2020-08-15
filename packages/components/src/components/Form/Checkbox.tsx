4ca77ecb9857159f55a0bbf88c19bf7cc1a2c5cf
sottar <ohrst.18@gmail.com>
2020-08-15 15:27:44 +0900

    feat(components): checkbox component

---
+++ packages/components/src/components/Form/Checkbox.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  checked: boolean;
+  disabled?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    padding-left: 8px;
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.65);
+  }
+`;
+
+const StyledCheckbox = styled.input`
+  position: relative;
+  width: 16px;
+  height: 16px;
+  appearance: none;
+  cursor: pointer;
+  background-color: #fff;
+  border-radius: 2px;
+  border-collapse: separate;
+  border: 1px solid #d9d9d9;
+  transition: all 0.3s;
+  &:checked {
+    background-color: #1890ff;
+    border-color: #1890ff;
+    &::after {
+      position: absolute;
+      display: table;
+      top: 50%;
+      left: 22%;
+      width: 5.71428571px;
+      height: 9.14285714px;
+      border: 2px solid #fff;
+      border-top: 0;
+      border-left: 0;
+      transform: rotate(45deg) scale(1) translate(-50%, -50%);
+      opacity: 1;
+      transition: all 0.2s cubic-bezier(0.12, 0.4, 0.29, 1.46) 0.1s;
+      content: ' ';
+    }
+  }
+  &:focus {
+    outline: 0;
+  }
+`;
+
+const Checkbox: React.FC<Props> = (props) => {
+  return (
+    <Wrapper className={props.className}>
+      <StyledCheckbox
+        type="checkbox"
+        checked={props.checked}
+        disabled={props.disabled}
+        onChange={props.onChange}
+      />
+      <span>{props.children}</span>
+    </Wrapper>
+  );
+};
+
+export default Checkbox;
+export { Checkbox };
