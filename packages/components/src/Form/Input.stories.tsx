340df6017b772456064b17e401a947673db721fe
Tatsushi K <>
2020-08-18 15:36:41 +0900

    test(Input): add stories

---
+++ packages/components/src/Form/Input.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { Input } from '.';
+import type { InputProps } from '.';
+
+export default {
+  title: 'Input',
+  component: Input,
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+  args: {},
+} as Meta<InputProps>;
+
+const Template: Story<InputProps> = (args) => <Input {...args} />;
+
+export const WithAddonBefore = Template.bind({});
+WithAddonBefore.args = {
+  addonBefore: 'before',
+  placeholder: 'Input with addonBefore',
+};
+
+export const WithAddonAfter = Template.bind({});
+WithAddonAfter.args = {
+  addonAfter: 'after',
+  placeholder: 'Input with addonAfter',
+};
