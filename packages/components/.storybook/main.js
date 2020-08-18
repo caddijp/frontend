d09f32d69f7d359ad521de74476dea8741ef6cac
Tatsushi K <>
2020-08-18 17:46:18 +0900

    Merge branch 'master' into add-stories

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -3,7 +3,7 @@
  * https://medium.com/storybookjs/declarative-storybook-configuration-49912f77b78
  */
 module.exports = {
-  stories: ['../src/**/*.(stories|story).tsx'],
+  stories: ['../src/**/*.stories.tsx', '../src/**/story.tsx'],
   addons: ['@storybook/addon-essentials'],
   // https://github.com/storybookjs/storybook/issues/11146#issuecomment-645341199
   typescript: {
