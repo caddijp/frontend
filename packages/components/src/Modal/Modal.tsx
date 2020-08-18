03a8bee4e0fe22001825ccad2aa450724901c9f1
scabyms <hiroaki_yamashita@caddi.jp>
2020-08-18 12:13:42 +0900

    Fix stylelint error

--- packages/components/src/Modal/Modal.tsx
+++ packages/components/src/Modal/Modal.tsx
@@ -1,13 +1,13 @@
 import { CloseOutlined } from '@ant-design/icons';
 import React from 'react';
-import ReactModal, { Props as ReactModalProps } from 'react-modal';
+import ReactModal, { Props as ReactModalProps, Styles as ReactModalStyles } from 'react-modal';
 import styled from 'styled-components';
 
 export interface ModalProps extends ReactModalProps {
   title: string;
 }
 
-const customStyles = {
+const customStyles: ReactModalStyles = {
   overlay: {
     background: 'rgba(72, 82, 109, 0.2)',
   },
@@ -16,46 +16,46 @@ const customStyles = {
 const StyledModal = styled(ReactModal)`
   position: absolute;
   top: 50%;
-  left: 50%;
   right: auto;
   bottom: auto;
-  margin-right: -50%;
-  transform: translate(-50%, -50%);
+  left: 50%;
   width: 568px;
   max-height: 80vh;
-  box-shadow: 0px 9px 28px rgba(0, 0, 0, 0.05), 0px 3px 6px rgba(0, 0, 0, 0.12),
-    0px 6px 16px rgba(0, 0, 0, 0.08);
-  border-radius: 2px;
+  margin-right: -50%;
   background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 9px 28px rgba(0, 0, 0, 0.05), 0 3px 6px rgba(0, 0, 0, 0.12),
+    0 6px 16px rgba(0, 0, 0, 0.08);
+  transform: translate(-50%, -50%);
 `;
 
 const StyledModalHeader = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
   width: 100%;
   height: 55px;
   background: #fff;
-  box-shadow: inset 0px -1px 0px #f0f0f0;
+  box-shadow: inset 0 -1px 0 #f0f0f0;
 `;
 
 const StyledModalTitle = styled.div`
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 22px;
   margin-left: 24px;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 22px;
   color: rgba(0, 0, 0, 0.85);
 `;
 
 const StyledCloseButton = styled.button`
   margin-right: 15px;
-  border: none;
   background: none;
+  border: none;
 `;
 
 const StyledModalBody = styled.div`
-  max-height: calc(80vh - 55px);
   width: 100%;
+  max-height: calc(80vh - 55px);
   background-color: #fff;
 `;
 
