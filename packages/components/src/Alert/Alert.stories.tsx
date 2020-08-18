9cf0997291f68f3018d6fb4b6933d1692026ea33
Tatsushi K <>
2020-08-18 15:54:02 +0900

    test(Alert): add stories

---
+++ packages/components/src/Alert/Alert.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { Alert } from '.';
+import type { AlertProps } from '.';
+
+export default {
+  title: 'Alert',
+  component: Alert,
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+  args: {},
+} as Meta<AlertProps>;
+
+const Template: Story<AlertProps> = (args) => <Alert {...args} />;
+
+export const Info = Template.bind({});
+Info.args = {
+  type: 'info',
+  message: 'information message',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  type: 'warning',
+  message: 'warning message',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  type: 'error',
+  message: 'error message',
+};
