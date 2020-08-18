2434038c51765f5c404f990b884c0c2a1775cf29
scabyms <hiroaki_yamashita@caddi.jp>
2020-08-18 19:50:31 +0900

    chore: improve_export_modal_sample
    Improve export modal sample in story

--- packages/components/src/Modal/story.DontTest.tsx
+++ packages/components/src/Modal/story.DontTest.tsx
@@ -1,36 +1,94 @@
 import { Story } from '@storybook/react';
 import React from 'react';
+import styled from 'styled-components';
 import { Modal, ModalProps } from '.';
+import { Button } from '../Button';
 
 export default {
   title: 'Modal',
   component: Modal,
 };
 
-const ExportModalContent = () => {
+const ExportModalChildrenWrapper = styled.div``;
+
+const ExportModalContent = styled.div`
+  top: 24px;
+  left: 24px;
+  width: 80%;
+  height: 60%;
+  color: rgba(0, 0, 0, 0.65);
+`;
+
+const ExportModalContentTitle = styled.span`
+  padding-inline-start: 40px;
+`;
+
+const ExportFilePageIdList = styled.ul`
+  margin-left: 0;
+  list-style: none;
+`;
+
+const ExportModalFooter = styled.div`
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 568px;
+  height: 53px;
+  border-radius: 2px;
+  box-shadow: inset 0 1px 0 #f0f0f0;
+`;
+
+const ExportModalInputArea = styled.div`
+  display: flex;
+  width: 386px;
+  height: 32px;
+`;
+
+const ExportModalInput = styled.input`
+  width: 100%;
+  font-size: 14px;
+  color: #bfbfbf;
+`;
+const ExportModalInputFileExtentionArea = styled.span`
+  width: 49px;
+  text-align: center;
+  background: #fafafa;
+  border: 1px solid #d9d9d9;
+`;
+const ExportModalButton = styled(Button)`
+  width: 144px;
+  height: 32px;
+`;
+
+const ExportModalChildren = () => {
   return (
-    <div style={{ padding: '30px' }}>
-      <div className="content">
-        <span>選択中の図面5件</span>
-        <ul>
+    <ExportModalChildrenWrapper>
+      <ExportModalContent>
+        <ExportFilePageIdList>
+          <li style={{ fontWeight: 'bold' }}>選択中の図面5件</li>
           <li>aaaa-aaaa-aaaa</li>
           <li>aaaa-aaaa-aaaa</li>
           <li>aaaa-aaaa-aaaa</li>
           <li>aaaa-aaaa-aaaa</li>
           <li>aaaa-aaaa-aaaa</li>
-        </ul>
-      </div>
-      <div className="exportButtonArea">
-        <input type="text" name="input" id="inputText" />
-        <button>test</button>
-      </div>
-    </div>
+        </ExportFilePageIdList>
+      </ExportModalContent>
+      <ExportModalFooter>
+        <ExportModalInputArea>
+          <ExportModalInput placeholder="ファイル名を入力※必須"></ExportModalInput>
+          <ExportModalInputFileExtentionArea>.pdf</ExportModalInputFileExtentionArea>
+        </ExportModalInputArea>
+        <ExportModalButton type="primary">エクスポートする</ExportModalButton>
+      </ExportModalFooter>
+    </ExportModalChildrenWrapper>
   );
 };
 
 const ExportTemplate: Story<ModalProps> = (args: ModalProps) => (
   <Modal {...args}>
-    <ExportModalContent />
+    <ExportModalChildren />
   </Modal>
 );
 
