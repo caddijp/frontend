0a0854203e8844db4649bf49e725aec01f5ac17f
scabyms <hiroaki_yamashita@caddi.jp>
2020-08-20 11:02:29 +0900

    fix: remove_unnecessary_style_statement
    Remove unnecessary style statement

--- packages/components/src/Pagination/Pagination.tsx
+++ packages/components/src/Pagination/Pagination.tsx
@@ -1,14 +1,10 @@
-import { Pagination as AntdPagination } from 'antd';
 import { PaginationProps as AntdPaginationProps } from 'antd/lib/pagination';
 import React from 'react';
-import styled from 'styled-components';
 
 export type PaginationProps = AntdPaginationProps;
 
-const StyledPagination = styled(AntdPagination)``;
-
 const Pagination: React.FC<PaginationProps> = (props) => {
-  return <StyledPagination {...props} />;
+  return <Pagination {...props} />;
 };
 
 export default Pagination;
