8bacae09a44de659c931b24b91e6f6676a0e3ef3
sottar <ohrst.18@gmail.com>
2020-09-19 21:18:46 +0900

    feat: update

--- packages/components/src/Upload/Upload.tsx
+++ packages/components/src/Upload/Upload.tsx
@@ -1,20 +1,11 @@
-import { InboxOutlined } from '@ant-design/icons';
 import { Upload as UploadComponent } from 'antd';
 import React from 'react';
 import { UploadProps } from './interface';
 
 const { Dragger } = UploadComponent;
 
-const Upload: React.FC<UploadProps> = (props) => {
-  return (
-    <Dragger {...props}>
-      <p className="ant-upload-drag-icon">
-        <InboxOutlined />
-      </p>
-      <p className="ant-upload-text">インポートするファイルをここにドラッグ&amp;ドロップ</p>
-      <p className="ant-upload-hint">※対応データ形式：.PDF</p>
-    </Dragger>
-  );
+const Upload: React.FC<UploadProps> = ({ children, ...props }) => {
+  return <Dragger {...props}>{children}</Dragger>;
 };
 
 export default Upload;
